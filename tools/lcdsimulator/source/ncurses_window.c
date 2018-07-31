81dfcc98e4a53c679fbff58871a77de13384ada9
rattata2me <jorgevegarias1@google.com>
2018-07-31 18:18:54 +0200

    1

--- tools/lcdsimulator/source/ncurses_window.c
+++ tools/lcdsimulator/source/ncurses_window.c
@@ -5,10 +5,22 @@ void ncurses_init(){
 	initscr();
 	noecho();
 
-
 }
 
 
 void ncurses_loop(G_Surface * buffer){
-	
+	char cBlock = (char)0x2588;
+	erase();
+	for(int y = 0; y < buffer->height; y++){
+		for(int x = 0; x < buffer->width; x++){
+			int e = g_get_pixel(buffer, x, y);
+			if(e == 0){
+				addch(0x20);
+			}else{
+				addch(cBlock);
+			}
+		}
+		printw("\n");
+	}
+	refresh();	
 }
